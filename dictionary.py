87c4e21ad50f0f96f6c46bff6aeb313022088154
gopal549 <59233919+gopal549@users.noreply.github.com>
2020-11-28 19:28:42 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+mydict = {
+    "Gopal" : "Raichand",
+    "Mohan" : "Student",
+    "marks" : [1,2,34,4],
+     1:2,
+     "anotherdict" :{ "davinder" : 'student'}
+}
+print("This is a dictionary :",mydict)
+print("This will gives only the keys of Dictionary : ",mydict.keys())
+print("This will gives only the values of keys",mydict.values())
+updatedict = {
+    "Shivani" : "friend",
+    "Amrita Kaur" : " friend"
+}
+mydict.update(updatedict)
+print(mydict)
